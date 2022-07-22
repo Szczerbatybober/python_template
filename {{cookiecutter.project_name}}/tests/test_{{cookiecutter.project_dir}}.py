64cb841f25e0b1ed5d876dc25cdb5154d9b2a701
Szczerbatybober <szczerbatybober@gmail.com>
2022-07-22 12:30:22 +0200

    Fix test imports

--- {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_dir}}.py
+++ {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_dir}}.py
@@ -7,7 +7,7 @@ import responses
 
 from {{cookiecutter.project_dir}} import __version__
 from {{cookiecutter.project_dir}}.logger import init_logger
-from {{cookiecutter.project_dir}}.sample.sample_class import SampleClass
+from {{cookiecutter.project_dir}}.greeter.greeter import Greeter
 
 
 def test_version():
@@ -29,17 +29,10 @@ def test_mock_request():
     sample_class = SampleClass(def_date=date.today())
     return_value = {
         "results": [
-            {
-                "name" : {
-                "first": "Test name"
-                }
-            }
+            {"name": {"first": "Test name", "last": "test last_name", "title": "mrs"}}
         ]
     }
-    responses.get(
-        url=sample_class.API_ENDPOINT,
-        json=return_value,
-        status=200
-    )
+    responses.get(url=sample_class.API_ENDPOINT, json=return_value, status=200)
 
     assert sample_class.get_random_name() == "Test name"
+
